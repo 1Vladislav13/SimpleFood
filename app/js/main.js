3cb6a6151c68017bd4117e62106cdb046c8bc562
1Vladislav13 <9093959@mail.ru>
2022-12-28 23:18:06 +0100

    add HW13

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
 $(function () {
 
+  $('.reviews__slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="slick-arrow__icon" width="18px" height="18px"><use href="./images/symbol-defs.svg#icon-prev-arrowLeft"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="slick-arrow__icon" width="18px" height="18px"><use href="./images/symbol-defs.svg#icon-prev-arrowRight"></use></svg></button>'
+  });
+
 });
 
 var mixer = mixitup('.category__products');
